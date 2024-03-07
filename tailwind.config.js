c1b6ecd89b1e8472e15c269b36d239153f07457c
Tetardtek01~ <kvnn64@gmail.com>
2024-03-07 11:36:18 +0100

    fix dark:

--- tailwind.config.js
+++ tailwind.config.js
@@ -36,10 +36,10 @@ plugins: [
   function({ addComponents }) {
     addComponents({
       '.btn': {
-        '@apply relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 bg-gradient-to-br from-purple-600 to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800' :{},
+        '@apply relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 bg-gradient-to-br from-purple-600 to-blue-500 hover:text-white text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800' :{},
       },
       '.btn-in': {
-        '@apply relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 group-hover:bg-opacity-0': {},
+        '@apply relative px-5 py-2.5 transition-all ease-in duration-75 bg-white bg-gray-900 group-hover:bg-opacity-0': {},
       },
       '.title1': {
         '@apply text-center text-8xl font-extrabold' :{},
